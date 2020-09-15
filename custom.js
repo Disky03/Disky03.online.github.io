0c46f6caae8d250b2b75163a09bf46ad3b3ae1a0
Disky03 <61417832+Disky03@users.noreply.github.com>
2020-09-15 15:22:39 +0700

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,42 @@
+// Function Hitung Mundur
+const tanggalTujuan = new Date('Sep 15, 2020 00:00:00').getTime();
+
+const hitungMundur = setInterval(function() {
+
+
+	const sekarang = new Date().getTime();
+	const selisih = tanggalTujuan - sekarang;
+
+	const jam = Math.floor(selisih % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+	const menit = Math.floor(selisih % (1000 * 60 * 60) / (1000 * 60));
+	const detik = Math.floor(selisih % (1000 * 60) / 1000);
+
+	const teksjam = document.getElementById('hours');
+	hours.innerHTML = jam;
+
+	const teksmenit = document.getElementById('minute');
+	minute.innerHTML = menit;
+
+	const teksdetik = document.getElementById('second');
+	second.innerHTML = detik;
+
+
+	if (selisih < 0) {
+		clearInterval(hitungMundur);
+		hours.innerHTML = 'H';
+		minute.innerHTML = 'B';
+		second.innerHTML = 'D';
+		tombol.style.visibility = 'visible';
+		teks.style.visibility = 'visible';
+	}
+
+}, 1000);
+// Akhir function hitung Mundur
+
+// tombol
+var tombol = document.querySelector('.get-started-btn');
+var teks = document.querySelector('.teks');
+
+function langsung() {
+	window.location = "cake.html";
+}
