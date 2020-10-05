433cf3633ebd9fc0652df34b04ed400c42142681
Disky03 <61417832+Disky03@users.noreply.github.com>
2020-10-05 19:57:09 +0700

    update custom.js

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 // Function Hitung Mundur
-const tanggalTujuan = new Date('Sep 16, 2020 00:00:00').getTime();
+const tanggalTujuan = new Date('Sep 1, 2021 00:00:00').getTime();
 
 const hitungMundur = setInterval(function() {
 
